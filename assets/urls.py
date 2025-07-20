70ea7d20584971c8cefb69babc71ad9208fdfe31
dk8534 <darshangupta18gba17@gmail.com>
2025-07-20 15:51:28 +0530

    Added create-admin route

--- assets/urls.py
+++ assets/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('create-admin/', views.create_admin),
     path('', views.dashboard, name='dashboard'),
     path('login/', views.login_view, name='login'),
     path('logout/', views.logout_view, name='logout'),
--- assets/views.py
+++ assets/views.py
@@ -356,3 +356,12 @@ def export_assets_excel(request):
 
     workbook.save(response)
     return response
+
+from django.contrib.auth.models import User
+from django.http import HttpResponse
+
+def create_admin(request):
+    if not User.objects.filter(username='admin').exists():
+        User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
+        return HttpResponse("✅ Admin user created.")
+    return HttpResponse("⚠️ Admin user already exists.")
